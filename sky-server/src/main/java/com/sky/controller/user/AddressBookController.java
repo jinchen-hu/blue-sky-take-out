cc9a5962a0129e0f6f55e5a1639a9f078ad87b59
Luke Hu <jinchenhu.luke@gmail.com>
2024-08-31 01:26:37 -0700

    address book

---
+++ sky-server/src/main/java/com/sky/controller/user/AddressBookController.java
@@ -0,0 +1,108 @@
+package com.sky.controller.user;
+
+import com.sky.context.BaseContext;
+import com.sky.entity.AddressBook;
+import com.sky.result.Result;
+import com.sky.service.AddressBookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user/addressBook")
+@Tag(name = "client side address book interfaces")
+public class AddressBookController {
+
+    @Autowired
+    private AddressBookService addressBookService;
+
+    /**
+     *
+     * @return
+     */
+    @GetMapping("/list")
+    @Operation
+    public Result<List<AddressBook>> list() {
+        AddressBook addressBook = new AddressBook();
+        addressBook.setUserId(BaseContext.getCurrentId());
+        List<AddressBook> list = addressBookService.list(addressBook);
+        return Result.success(list);
+    }
+
+    /**
+     *
+     * @param addressBook
+     * @return
+     */
+    @PostMapping
+    @Operation
+    public Result save(@RequestBody AddressBook addressBook) {
+        addressBookService.save(addressBook);
+        return Result.success();
+    }
+
+    @GetMapping("/{id}")
+    @Operation
+    public Result<AddressBook> getById(@PathVariable Long id) {
+        AddressBook addressBook = addressBookService.getById(id);
+        return Result.success(addressBook);
+    }
+
+    /**
+     *
+     * @param addressBook
+     * @return
+     */
+    @PutMapping
+    @Operation
+    public Result update(@RequestBody AddressBook addressBook) {
+        addressBookService.update(addressBook);
+        return Result.success();
+    }
+
+    /**
+     *
+     * @param addressBook
+     * @return
+     */
+    @PutMapping("/default")
+    @Operation
+    public Result setDefault(@RequestBody AddressBook addressBook) {
+        addressBookService.setDefault(addressBook);
+        return Result.success();
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    @DeleteMapping
+    @Operation
+    public Result deleteById(Long id) {
+        addressBookService.deleteById(id);
+        return Result.success();
+    }
+
+    /**
+     */
+    @GetMapping("default")
+    @Operation
+    public Result<AddressBook> getDefault() {
+        //SQL:select * from address_book where user_id = ? and is_default = 1
+        AddressBook addressBook = new AddressBook();
+        addressBook.setIsDefault(1);
+        addressBook.setUserId(BaseContext.getCurrentId());
+        List<AddressBook> list = addressBookService.list(addressBook);
+
+        if (list != null && list.size() == 1) {
+            return Result.success(list.get(0));
+        }
+
+        return Result.error("No default address");
+    }
+
+}
---
+++ sky-server/src/main/java/com/sky/mapper/AddressBookMapper.java
@@ -0,0 +1,52 @@
+package com.sky.mapper;
+
+import com.sky.entity.AddressBook;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface AddressBookMapper {
+
+    /**
+     * @param addressBook
+     * @return
+     */
+    List<AddressBook> list(AddressBook addressBook);
+
+    /**
+     * 新增
+     * @param addressBook
+     */
+    @Insert("insert into address_book" +
+            "        (user_id, consignee, phone, sex, province_code, province_name, city_code, city_name, district_code," +
+            "         district_name, detail, label, is_default)" +
+            "        values (#{userId}, #{consignee}, #{phone}, #{sex}, #{provinceCode}, #{provinceName}, #{cityCode}, #{cityName}," +
+            "                #{districtCode}, #{districtName}, #{detail}, #{label}, #{isDefault})")
+    void insert(AddressBook addressBook);
+
+    /**
+     * @param id
+     * @return
+     */
+    @Select("select * from address_book where id = #{id}")
+    AddressBook getById(Long id);
+
+    /**
+     * 根据id修改
+     * @param addressBook
+     */
+    void update(AddressBook addressBook);
+
+    /**
+     * @param addressBook
+     */
+    @Update("update address_book set is_default = #{isDefault} where user_id = #{userId}")
+    void updateIsDefaultByUserId(AddressBook addressBook);
+
+    /**
+     * @param id
+     */
+    @Delete("delete from address_book where id = #{id}")
+    void deleteById(Long id);
+}
